658c8b868518ecea4c6aad04c9176ea4dd076741
greencrabby <j0shuaelishashalom@gmail.com>
2025-03-21 12:49:41 +0700

    (Bonus) Function improvement

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ use std::{
 pub struct ThreadPool {
     workers: Vec<Worker>,
     sender: mpsc::Sender<Job>,
-}
+    }
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
 
@@ -30,6 +30,21 @@ impl ThreadPool {
     {
         let job = Box::new(f);
         self.sender.send(job).unwrap();
+    }
+
+    pub fn build(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        ThreadPool { workers, sender }
     }
 }
 
